1c57512a85681037248d3406c082032411a90f2d
Minh Tran <trcongminh.261002@gmail.com>
2025-12-05 10:18:49 +0700

    feat: implement webhook handling for user creation, update, and deletion

---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,58 @@
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+import type { User } from "@prisma/client";
+import { db } from "@/lib/db";
+import { clerkClient } from "@clerk/nextjs/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+    const eventType = evt.type;
+
+    if (eventType === "user.created" || eventType === "user.updated") {
+      const data = evt.data;
+      const user: Partial<User> = {
+        id: data.id,
+        name: `${data.first_name} ${data.last_name}`,
+        email: data.email_addresses?.[0]?.email_address,
+        picture: data.image_url,
+      };
+      if (!user) return;
+
+      const dbUser = await db.user.upsert({
+        where: {
+          email: user.email,
+        },
+        update: user,
+        create: {
+          id: user.id!,
+          name: user.name!,
+          email: user.email!,
+          picture: user.picture!,
+          role: user.role || "USER",
+        },
+      });
+
+      const client = await clerkClient();
+      client.users.updateUserMetadata(data.id, {
+        privateMetadata: {
+          role: dbUser.role || "USER",
+        },
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      const data = evt.data;
+      await db.user.deleteMany({
+        where: {
+          id: data.id,
+        },
+      });
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
